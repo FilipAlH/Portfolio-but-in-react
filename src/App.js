979e4f111f02afa4bf11c9761005744d117f4119
Filip <f2alhama@uwaterloo.ca>
2021-11-22 02:05:22 -0500

    made header and footer into components

--- src/App.js
+++ src/App.js
@@ -3,27 +3,21 @@ import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 import Home from './pages/Home.js';
 import Projects from './pages/Projects.js'
+import Footer from './components/footer.js'
+import Header from './components/header.js'
 
 
 function App() {
   return (
     <Router>
-        <div className="row-12"> 
-          <ul className="nav nav-tabs colorP" id="myTab" role="tablist">
-            <li className="nav-item" role="presentation">
-                <Link className="nav-link active" id="home-tab" to={'/'} aria-selected="true">About me</Link>
-            </li>
-            <li className="nav-item dropdown" role="presentation">
-                <Link className="nav-link dropdown-toggle disDropdown" id="profile-tab" to={'/Projects'} aria-expanded="false">Projects</Link>
-            </li>
-          </ul>
-        </div>
+      <Header />
         <Route exact path="/">
           <Home />
         </Route>
         <Route exact path="/Projects">
           <Projects />
         </Route>
+      <Footer />
     </Router>
   );
 }
